48c8f04ba48d12bda6ee1341e48674295231095d
Faiyyaz25003 <khanwork25003@gmail.com>
2025-12-29 19:47:49 +0530

    profile

--- Controller/userController.js
+++ Controller/userController.js
@@ -125,3 +125,18 @@ export const toggleBlockUser = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteUser,
   toggleBlockUser,
   loginUser,
+  getUserById,
 } from "../Controller/UserController.js";
 
 const router = express.Router();
@@ -14,6 +15,10 @@ router.post("/register", registerUser);
 // Login
 router.post("/login", loginUser);
 router.get("/", getUsers);
+
+router.get("/:id", getUserById);
+
+
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 router.put("/block/:id", toggleBlockUser);
